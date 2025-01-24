994ae6c921485c753883cb44bb0590241773d3f7
kundan <kundanbora77@gmail.com>
2025-01-24 13:54:52 +0530

    updated config file

--- src/config/config.php
+++ src/config/config.php
@@ -1,17 +1,17 @@
 <?php
 return [
     'ftp' => [
-        'host' => 'your_sftp_host', // Example: 'sftp4.logiq.no'
-        'port' => 22, // Example: 2222
-        'username' => 'your_sftp_username', // Example: 'hartmnrdc'
-        'password' => 'your_sftp_password', // Example: 'iWmshPu7d'
+        'host' => 'your_sftp_host',
+        'port' => 22, 
+        'username' => 'your_sftp_username', 
+        'password' => 'your_sftp_password', 
     ],
     'rackbeat' => [
         'api_url' => 'https://api.rackbeat.com', 
-        'api_key' => 'your_rackbeat_api_key', // Example: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9...'
+        'api_key' => 'your_rackbeat_api_key',
     ],
     'order' => [
-        'remote_order_directory' => 'your_remote_order_directory', // Example: '/2hartmnrdc'
-        'remote_confirmation_directory' => 'your_remote_confirmation_directory', // Example: '/2logiq'
+        'remote_order_directory' => 'your_remote_order_directory', 
+        'remote_confirmation_directory' => 'your_remote_confirmation_directory', 
     ],
 ];
