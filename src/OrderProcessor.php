b1d1f1986335d2e853e5b3b31e4fde28b154878b
kundan <kundanbora77@gmail.com>
2025-02-07 12:04:08 +0530

    updated like advance order response

--- src/OrderProcessor.php
+++ src/OrderProcessor.php
@@ -201,68 +201,84 @@ class OrderProcessor
             file_put_contents($orderStatusFile, json_encode($orders, JSON_PRETTY_PRINT));
         }
     }
-
     public function createOrderResponse($rackbeatOrder, $originalOrderFile) 
     {
         $responseDir = __DIR__ . '/../order-response';
         if (!is_dir($responseDir)) {
             mkdir($responseDir, 0777, true);
         }
-
-        // Create XML document
-        $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><OrderResponse></OrderResponse>');
-        
-        // Add namespaces
-        $xml->addAttribute('xmlns', 'urn:oasis:names:specification:ubl:schema:xsd:OrderResponse-2');
-        $xml->addAttribute('xmlns:cac', 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2');
-        $xml->addAttribute('xmlns:cbc', 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2');
-
-        // Add required elements
-        $xml->addChild('cbc:CustomizationID', 'urn:fdc:peppol.eu:poacc:trns:order_response_advanced:3');
-        $xml->addChild('cbc:ProfileID', 'urn:fdc:peppol.eu:poacc:bis:advanced_ordering:3');
-        $xml->addChild('cbc:ID', $rackbeatOrder['number']);
-        $xml->addChild('cbc:IssueDate', date('Y-m-d'));
-        $xml->addChild('cbc:IssueTime', date('H:i:s'));
-        
-        // Set response code (CA = Change)
-        $xml->addChild('cbc:OrderResponseCode', 'CA');
-        
-        // Add currency
-        $xml->addChild('cbc:DocumentCurrencyCode', $rackbeatOrder['currency']);
-
-        // Add seller party
-        $sellerParty = $xml->addChild('cac:SellerSupplierParty')->addChild('cac:Party');
-        $sellerParty->addChild('cbc:EndpointID', '7598000000128')->addAttribute('schemeID', '0088');
-        
-        // Add buyer party
-        $buyerParty = $xml->addChild('cac:BuyerCustomerParty')->addChild('cac:Party');
-        $buyerParty->addChild('cbc:EndpointID', $rackbeatOrder['customer']['ean'])->addAttribute('schemeID', '0088');
-        
-        // Add order lines
-        foreach ($rackbeatOrder['lines'] as $line) {
-            $orderLine = $xml->addChild('cac:OrderLine');
-            $lineItem = $orderLine->addChild('cac:LineItem');
-            
-            $lineItem->addChild('cbc:ID', $line['id']);
-            $lineItem->addChild('cbc:Quantity', $line['quantity'])->addAttribute('unitCode', 'C62');
-            
-            // Add price
-            $price = $lineItem->addChild('cac:Price');
-            $price->addChild('cbc:PriceAmount', $line['line_price'])->addAttribute('currencyID', $rackbeatOrder['currency']);
-            
-            // Add item details
-            $item = $lineItem->addChild('cac:Item');
-            $item->addChild('cbc:Name', $line['name']);
-            
-            // Add seller's item identification
-            $sellersItem = $item->addChild('cac:SellersItemIdentification');
-            $sellersItem->addChild('cbc:ID', $line['item']['number']);
+    
+        // Load original order
+        $originalXml = simplexml_load_file($originalOrderFile);
+        $originalXml->registerXPathNamespace('cbc', 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2');
+        $originalXml->registerXPathNamespace('cac', 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2');
+    
+        // Create XML with correct structure
+        $xmlString = '<?xml version="1.0" encoding="UTF-8"?>' .
+            '<OrderResponse 
+                xmlns="urn:oasis:names:specification:ubl:schema:xsd:OrderResponse-2" 
+                xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" 
+                xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">' .
+            '<cbc:CustomizationID>urn:fdc:peppol.eu:poacc:trns:order_response_advanced:3</cbc:CustomizationID>' .
+            '<cbc:ProfileID>urn:fdc:peppol.eu:poacc:bis:advanced_ordering:3</cbc:ProfileID>' .
+            '<cbc:ID>' . $rackbeatOrder['number'] . '</cbc:ID>' .
+            '<cbc:IssueDate>' . date('Y-m-d') . '</cbc:IssueDate>' .
+            '<cbc:IssueTime>' . date('H:i:s') . '</cbc:IssueTime>' .
+            '<cbc:OrderResponseCode>CA</cbc:OrderResponseCode>' . // Changed from AP to CA
+            '<cbc:DocumentCurrencyCode>' . $rackbeatOrder['currency'] . '</cbc:DocumentCurrencyCode>' .
+            '<cac:OrderReference>' .
+            '<cbc:ID>' . (string)$originalXml->xpath('//cbc:ID')[0] . '</cbc:ID>' .
+            '</cac:OrderReference>' .
+            '<cac:SellerSupplierParty>' .
+            '<cac:Party>' .
+            '<cbc:EndpointID schemeID="0088">7080010019356</cbc:EndpointID>' .
+            '<cac:PartyIdentification>' .
+            '<cbc:ID schemeID="0192">997066588</cbc:ID>' .
+            '</cac:PartyIdentification>' .
+            '<cac:PartyLegalEntity>' .
+            '<cbc:RegistrationName>HARTMAN NORDIC AS</cbc:RegistrationName>' .
+            '</cac:PartyLegalEntity>' .
+            '</cac:Party>' .
+            '</cac:SellerSupplierParty>' .
+            '<cac:BuyerCustomerParty>' .
+            '<cac:Party>' .
+            '<cbc:EndpointID schemeID="0088">' . (string)$originalXml->xpath('//cac:BuyerCustomerParty//cbc:EndpointID')[0] . '</cbc:EndpointID>' .
+            '<cac:PartyLegalEntity>' .
+            '<cbc:RegistrationName>' . (string)$originalXml->xpath('//cac:BuyerCustomerParty//cac:PartyName/cbc:Name')[0] . '</cbc:RegistrationName>' .
+            '</cac:PartyLegalEntity>' .
+            '</cac:Party>' .
+            '</cac:BuyerCustomerParty>';
+    
+        // Add order lines only if response code is CA
+        foreach ($rackbeatOrder['lines'] as $lineIndex => $line) {
+            $originalLineId = $lineIndex + 1; // Get original line ID
+            $xmlString .= '<cac:OrderLine>' .
+                '<cac:LineItem>' .
+                '<cbc:ID>' . $line['id'] . '</cbc:ID>' .
+                '<cbc:LineStatusCode>5</cbc:LineStatusCode>' . // Using valid UNCL1229 code
+                '<cbc:Quantity unitCode="EA">' . $line['quantity'] . '</cbc:Quantity>' .
+                '<cac:Price>' .
+                '<cbc:PriceAmount currencyID="' . $rackbeatOrder['currency'] . '">' . $line['line_price'] . '</cbc:PriceAmount>' .
+                '</cac:Price>' .
+                '<cac:Item>' .
+                '<cbc:Name>' . htmlspecialchars($line['name'], ENT_XML1) . '</cbc:Name>' .
+                '<cac:SellersItemIdentification>' .
+                '<cbc:ID>' . $line['item']['number'] . '</cbc:ID>' .
+                '</cac:SellersItemIdentification>' .
+                '</cac:Item>' .
+                '</cac:LineItem>' .
+                '<cac:OrderLineReference>' .
+                '<cbc:LineID>' . $originalLineId . '</cbc:LineID>' .
+                '</cac:OrderLineReference>' .
+                '</cac:OrderLine>';
         }
-
-        // Save the response file
+    
+        $xmlString .= '</OrderResponse>';
+    
+        // Save file
         $responseFilename = basename($originalOrderFile);
         $responseFilePath = $responseDir . '/' . $responseFilename;
-        $xml->asXML($responseFilePath);
+        file_put_contents($responseFilePath, $xmlString);
         
         return $responseFilePath;
     }
